28a79e093b335d1f143de3b13f3a8c6ceb688cb9
Boao Huang <36747993+BoXun97@users.noreply.github.com>
2020-12-07 17:46:41 +0800

    Add script for future test
    It is a new rotation optional script for future to use.

---
+++ unityAR_Implementation/AR_All/Assets/Script/NewRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class NewRotation : MonoBehaviour, IDragHandler
+{
+
+	public Transform target;
+	float speed = .3f;
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		Vector3 Vec3rote = new Vector3(eventData.delta.y, -eventData.delta.x);
+		target.Rotate(Vec3rote * speed, Space.World);
+
+	}
+}
