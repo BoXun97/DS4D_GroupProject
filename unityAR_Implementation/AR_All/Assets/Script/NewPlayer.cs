5c249058f0d84d631e825a0d62105149bd6d41a1
Boao Huang <36747993+BoXun97@users.noreply.github.com>
2020-12-07 18:06:27 +0800

    Show Model in future
    the way to show the model of LDA can be used

---
+++ unityAR_Implementation/AR_All/Assets/Script/NewPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class NewPlayer : MonoBehaviour, IDragHandler
+{
+    void Start(){
+
+    }
+
+    void Update() {
+
+        if(transform.localPosition.y > 0)
+        {
+            return;
+        
+        }
+        transform.Translate(new Vector3(0,0.7,0) * Time.deltaTime);
+        
+    }
+}
